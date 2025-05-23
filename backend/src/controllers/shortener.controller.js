6141752a6c0d09daa68c7bdf69336882551e1b39
saplynx <saplynx@gmail.com>
2025-05-23 16:08:51 +0530

    added shortener logic

---
+++ backend/src/controllers/shortener.controller.js
@@ -0,0 +1,30 @@
+const ShortUniqueId = require("short-unique-id");
+const { randomUUID } = new ShortUniqueId({ length: 6 });
+const duplicateChecker = require("../utils/duplicateChecker");
+const Url = require("../models/url.model");
+const { DOMAIN } = require("../config/env");
+
+async function shortener(req, res, next) {
+    try {
+        let shortId = randomUUID();
+        while (await duplicateChecker(shortId)) {
+            shortId = randomUUID();
+        }
+
+        const newUrl = new Url({
+            shortId: shortId,
+            longUrl: req.body.longUrl,
+        });
+
+        newUrl.save();
+
+        return res.status(201).json({
+            shortId: shortId,
+            shortUrl: DOMAIN + "/" + shortId,
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = shortener;
