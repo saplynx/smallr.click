88f96d87f1cdec828095d79f42fcefda617d4384
saplynx <saplynx@gmail.com>
2025-05-23 16:14:47 +0530

    removed mongodb config

--- backend/src/config/mongodb.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    dev_connection_url:
-        "mongodb+srv://saplynx:H4JXIYGDgwTXtGNx@cluster0.oqcn4ap.mongodb.net/dev",
-    prod_connection_url:
-        "mongodb+srv://saplynx:H4JXIYGDgwTXtGNx@cluster0.oqcn4ap.mongodb.net/prod",
-};
