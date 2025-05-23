3ce57a0a1643da408bcf0f7a5cd4713bab3e8acf
saplynx <saplynx@gmail.com>
2025-05-23 16:08:33 +0530

    updated configs

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,4 +1,5 @@
 module.exports = {
     API_VERSION: "v1",
     PORT: 5000,
+    DOMAIN: "https://smallr.click",
 };
---
+++ backend/src/config/mongodb.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    dev_connection_url:
+        "mongodb+srv://saplynx:H4JXIYGDgwTXtGNx@cluster0.oqcn4ap.mongodb.net/dev",
+    prod_connection_url:
+        "mongodb+srv://saplynx:H4JXIYGDgwTXtGNx@cluster0.oqcn4ap.mongodb.net/prod",
+};
